64aa46a107c274bf401a7c4d4dea2d2241d6e6d3
Empa Emanuel <empaemanuel@gmail.com>
2019-12-04 16:45:53 +0100

    Lagt till circular prevention till prototypbaserade.

--- ProtoMultInherit.js
+++ ProtoMultInherit.js
@@ -1,5 +1,4 @@
 let myObject = {
-
 	call: function(funcName, parameters){
 		if (this.hasOwnProperty(funcName))
 			return this[funcName](parameters);
@@ -10,21 +9,39 @@ let myObject = {
 		}
 	},
 
-	create: function(prototypeList,name){
-		return { 
-			__proto__: myObject,
-			name,
-			prototypeList,
-		};
+	create: function(prototypeList){
+		let newObj = Object.create(this);
+		newObj.prototypeList = prototypeList;
+		return newObj;
+	},
+
+	addPrototype: function(prototype){
+		if(!prototype.hasPrototype(this))
+			if(this.prototypeList === null){
+				this.prototypeList = [prototype];
+			} else {
+				this.prototypeList.push(prototype);
+			};
+	},
+
+	hasPrototype: function(prototype){
+		if(this.prototypeList === null)
+			return false;
+		for(let proto of this.prototypeList){
+			if(proto === prototype)
+				return true;
+			if(proto.hasPrototype(prototype))
+				return true;
+		} return false;
 	}
 };
+
 
 
 obj0 = myObject.create(null);
 obj0.func = function(arg) { return "func0: " + arg; };
 result = obj0.call("func", ["hello"]);
 console.log("should print ’func0: hello’ ->", result);
-
 
 obj0 = myObject.create(null, 'obj0');
 obj0.func = function(arg) { return "func0: " + arg; };
@@ -34,7 +51,6 @@ obj3 = myObject.create([obj2, obj1], 'obj3');
 result = obj3.call("func", ["hello"]);
 console.log("should print ’func0: hello’ ->", result);
 
-
 var obj0 = myObject.create(null);
 obj0.func = function(arg) { return "func0: " + arg; };
 var obj1 = myObject.create([obj0]);
@@ -43,3 +59,9 @@ obj2.func = function(arg) { return "func2: " + arg; };
 var obj3 = myObject.create([obj1, obj2]);
 var result = obj3.call("func", ["hello"]);
 console.log("should print ’func0: hello’ ->", result);
+
+var obj0 = myObject.create(null);
+var obj1 = myObject.create([obj2]);
+obj2.addPrototype(obj3);
+obj3.addPrototype(obj0);
+console.log(obj3.prototypeList);
